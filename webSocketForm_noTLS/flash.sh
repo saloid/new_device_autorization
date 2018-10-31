def856e266912c861d8a2f33a852f7e9f2ec8d2e
Saloid <svetotled@gmail.com>
2018-10-31 22:40:41 +0200

    Removed -cz option

--- webSocketForm_noTLS/flash.sh
+++ webSocketForm_noTLS/flash.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/home/qitment/.arduino15/packages/esp8266/tools/esptool/0.4.12/esptool -vv -cd nodemcu -cb 921600 -cp /dev/ttyUSB0 -ca 0x0 -cz 0x100000 -ca 0x00000 -cf webSocketForm_noTLS.ino.generic.bin
+/home/qitment/.arduino15/packages/esp8266/tools/esptool/0.4.12/esptool -vv -cd nodemcu -cb 921600 -cp /dev/ttyUSB0 -ca 0x00000 -cf webSocketForm_noTLS.ino.generic.bin
